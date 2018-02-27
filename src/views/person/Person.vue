267bc3f0a9dfec4d6b627b20aba8e4a03c6e05a9
jiadongchao <18519145255@163.com>
2018-02-27 18:56:19 +0800

    增加echarts图表

--- src/views/person/Person.vue
+++ src/views/person/Person.vue
@@ -22,9 +22,56 @@
                 </li>                
             </ul>
         </div>
+        <div class="echartsWrap">
+            <div class="echarts" id="myChart" :style="{width: '300px', height: '300px'}"></div>
+        </div>
     </div>
   </div>
 </template>
+<script>
+// 引入基本模板
+let echarts = require('echarts/lib/echarts')
+// 引入柱状图组件
+require('echarts/lib/chart/bar')
+// 引入提示框和title组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
+export default{
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+        this.drawLine()
+    },
+    methods:{
+        drawLine() {
+            // 基于准备好的dom，初始化echarts实例
+            let myChart = echarts.init(document.getElementById('myChart'))
+            // 绘制图表
+            myChart.setOption({
+                title: { text: 'ECharts 入门示例' },
+                tooltip: {},
+                xAxis: {
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                },
+                yAxis: {},
+                series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+                }]
+            });
+        }  
+    }
+}
+
+
+
+
+
+</script>
 
 <style scoped>
 .person{height: 100%;position: relative;padding:30px 50px 0 50px;}
@@ -37,4 +84,5 @@
 .contentItem_li_3{display: inline-block;width: 28%;background: #409EFF;padding-left:20px;  margin:0 50px 30px 0;float: left;height: 250px;}
 .contentItem_span{font-size: 22px;text-align: center;padding-top: 45px}
 
+.echarts{width: 300px;height: 200px;}
 </style>
